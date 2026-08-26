22a6b99ecc9adbc15f80595ea45357b27f0fb02d
John Doe <205268470+ihuynhpersonal@users.noreply.github.com>
2026-08-26 12:25:52 -0500

    Fix Terraform side of deploy and run script

--- terraform/modules/server/server.tf
+++ terraform/modules/server/server.tf
@@ -29,8 +29,8 @@ resource "oci_core_instance" "palworld_server" {
 
 resource "local_file" "ansible_hosts" {
     filename = "${path.root}/../ansible/hosts.ini"
-    content = templatefile("${path.root}/../ansible/hosts.ini.tpl", {
-        web_ips = [oci_core_instance.palworld_server.private_ip]
+    content = templatefile("${path.module}/templates/hosts.ini.tpl", {
+        ip = oci_core_instance.palworld_server.private_ip
         ssh_private_key_path = var.ssh_private_key_path
     })
 }
